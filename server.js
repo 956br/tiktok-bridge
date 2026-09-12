3eaebc181e13a05c1717a1f6857347caa80367d0
956br <gh.kh.00062@gmail.com>
2026-09-12 17:51:14 +0300

    Update server.js

--- server.js
+++ server.js
@@ -23,14 +23,32 @@ wss.on('connection', (ws, req) => {
 
   async function start() {
     tiktokLive = new TikTokLiveConnection(username, {
-      signApiKey: API_KEY
+      signApiKey: API_KEY,
+      enableExtendedGiftInfo: true
     });
 
+    // التعليقات
     tiktokLive.on(WebcastEvent.CHAT, data => {
       if (ws.readyState === ws.OPEN) {
         const comment = data?.comment ?? data?.content;
         ws.send(JSON.stringify({ user: data?.user?.nickname, comment }));
       }
+    });
+
+    // الهدايا
+    tiktokLive.on(WebcastEvent.GIFT, data => {
+      if (ws.readyState !== ws.OPEN) return;
+      const giftType = data?.giftDetails?.giftType;
+      if (giftType === 1 && !data?.repeatEnd) return;
+      ws.send(JSON.stringify({
+        type: 'gift',
+        user: data?.user?.nickname,
+        giftName: data?.giftDetails?.giftName,
+        giftId: data?.giftId,
+        giftType: giftType,
+        diamonds: data?.giftDetails?.diamondCount,
+        count: data?.repeatCount || 1
+      }));
     });
 
     tiktokLive.on('disconnected', () => {
